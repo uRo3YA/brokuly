d4abbdb573bb1207e0a3d6c8b09eb54321d8fb23
qlghwp123 <qlghwp123@naver.com>
2022-11-11 11:19:13 +0900

    feature : Add accounts/wishlist wishlist 기능 추가

--- accounts/urls.py
+++ accounts/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('logout/', views.logout, name='logout'),
     path('mypage/', views.mypage, name='mypage'),
     path('cart/', views.cart, name='cart'),
-    path('cart/add/<int:product_id>/', views.add_cart, name='add_cart')
+    path('cart/add/<int:product_id>/', views.add_cart, name='add_cart'),
+    path('wishlist/', views.wishlist,name='wishlist')
 ]
