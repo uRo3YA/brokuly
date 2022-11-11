f2d3b381cb31a8498303640459f02a7c5840de14
qlghwp123 <qlghwp123@naver.com>
2022-11-11 14:09:34 +0900

    feature : Add accounts/review and etc accounts/review 기능 및 url 추가 해당 기능 테스트 위한 review.html 추가 Product, Review 테이블에 user 필드 추가 필드 추가에 따른 products, reviews 앱 내 views.py 내 create 부분 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,5 +13,6 @@ urlpatterns = [
     path('mypage/', views.mypage, name='mypage'),
     path('cart/', views.cart, name='cart'),
     path('cart/add/<int:product_id>/', views.add_cart, name='add_cart'),
-    path('wishlist/', views.wishlist,name='wishlist')
+    path('wishlist/', views.wishlist,name='wishlist'),
+    path('review/', views.review, name='review'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -8,6 +8,7 @@ from .models import Review
 def index(request):
     reviews = Review.objects.order_by("-pk")
     context = {"reviews": reviews}
+
     return render(request, "reviews/index.html", context)
 
 
@@ -19,6 +20,7 @@ def detail(request, pk):
         #    'comments': review.comment_set.all(),
         #    'comment_form': comment_form,
     }
+
     return render(request, "reviews/detail.html", context)
 
 
@@ -27,24 +29,32 @@ def review_create(request, pk):
     if request.method == "POST":
         # DB에 저장하는 로직
         review_form = ReviewForm(request.POST)
+
         if review_form.is_valid():
             new = review_form.save(commit=False)
             new.product_id = info.pk
-            # new.user = request.user
+            new.user = request.user
             new.save()
+
             return redirect("products:detail", info.pk)
     else:
         review_form = ReviewForm()
-    context = {"review_form": review_form}
+
+    context = {
+        "review_form": review_form
+    }
+
     return render(request, "reviews/form.html", context=context)
 
 
 def review_detail(request, product_pk, review_pk):
     info = Review.objects.get(pk=review_pk)
     # comment_form = CommentForm()
+
     context = {
         "info": info,
         # "comment_form": comment_form,
         # "comments": info.comment_set.all(),
     }
+
     return render(request, "reviews/detail.html", context)
