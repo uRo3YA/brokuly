7a64557e1a09b2543b26b652d46256a606f1a930
LeeTaeGeuk <108652200+uRo3YA@users.noreply.github.com>
2022-11-13 21:02:30 +0900

    feature : 검색 기능 추가 - 상품의 제목or설명중 포함된 키워드 검색되게 함

--- products/urls.py
+++ products/urls.py
@@ -9,8 +9,9 @@ urlpatterns = [
     path("<int:pk>/", views.detail, name="detail"),
     path("<int:pk>/update/", views.update, name="update"),
     path("<int:pk>/delete/", views.delete, name="delete"),
-    path("add_cart/<int:pk>", views.add_cart, name="add_cart"),
+    path("search", views.search, name="search"),
+    #############################################
     # path("cart/", views.cart, name="cart")
-    # path('search', views.search, name='search'),
+    path("add_cart/<int:pk>", views.add_cart, name="add_cart"),
     # path('<int:pk>/wishlist/', views.wishlist),
 ]
