f9fa98f1a17c81440b14506019a2553d325aa9c0
wnsn8546 <wnsn8546@gmail.com>
2022-11-14 03:11:25 +0900

    feature : qnas/CRUD , 소셜공유 아이콘 및 기능 추가 question,answer CRUD, facebook, twitter만 우선 작동

--- orders/urls.py
+++ orders/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
+
+app_name = 'orders'
 
 urlpatterns = [
-
+    # path('orders/<int:order_id>/상품목록?/create/', views.orders_create, name='create_order'),
 ]
